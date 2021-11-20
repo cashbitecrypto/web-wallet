287ea42968ba533ed7e70a5116e67c753fe812a9
Jake Walker <hi@jakew.me>
2021-11-20 16:56:23 +0000

    Add skeleton loaders

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,7 +2,7 @@ import { useUser } from '@auth0/nextjs-auth0'
 import { gql, useMutation, useQuery } from '@apollo/client';
 import type { NextPage } from 'next'
 import Layout from '../components/layout';
-import { Avatar, Box, Container, IconButton, List, ListItem, ListItemAvatar, ListItemText, Typography, Paper } from '@mui/material';
+import { Avatar, Box, Container, IconButton, List, ListItem, ListItemAvatar, ListItemText, Typography, Paper, Skeleton } from '@mui/material';
 import { CallMade, CallReceived, CheckCircle, OpenInNew, TrackChanges } from '@mui/icons-material';
 import { green, orange, red } from '@mui/material/colors';
 import Transaction from '../components/transaction';
@@ -28,35 +28,46 @@ const WalletQuery = gql`
 
 const Home: NextPage = () => {
   const query = useQuery(WalletQuery);
+  const loading = query.loading || query.error || !query.data.wallet;
 
   let content;
 
-  if (query.loading) {
-    content = <p>Loading...</p>;
-  } else if (query.error) {
+  if (query.error) {
     content = <>
       <p>Something went wrong: {query.error.toString()}</p>
       <p>Got a 401 error? Please go to the settings tab to login.</p>
     </>;
-  } else if (!query.data.wallet) {
-    content = <p>You have no wallet. Please go to the settings page to make one.</p>;
   } else {
     content = <>
       <Box sx={{ backgroundColor: '#9013fe', color: 'white', margin: 0, paddingY: 10, textAlign: 'center' }}>
         <Container maxWidth="lg">
-          <Typography variant="h2">{query.data.wallet.unlockedBalance} CB</Typography>
+          <Typography variant="h2">
+            {loading
+              ? <Skeleton width={200} sx={{ marginX: 'auto' }} />
+              : `${query.data.wallet.unlockedBalance} CB`
+            }
+          </Typography>
           <Typography variant="overline">Available Balance</Typography>
         </Container>
       </Box>
       <Container sx={{ marginY: 3 }}>
         <Paper variant="outlined">
           <List sx={{ bgcolor: 'background.paper', overflowX: 'clip' }}>
-            {
-              query.data.wallet.transactions.length > 0
-                ? query.data.wallet.transactions.map((tx: any) => (
-                    <Transaction key={tx.hash} transaction={tx}/>
-                  ))
-                : <Typography variant="caption" sx={{ paddingLeft: 3 }}>No transactions yet</Typography>
+            {loading
+              ? [...Array(10)].map((i) => (
+                  <ListItem key={i}>
+                    <ListItemAvatar>
+                      <Skeleton variant="circular" width={40} height={40} />
+                    </ListItemAvatar>
+                    <ListItemText primary={<Skeleton width={150}/>} secondary={<Skeleton width={250}/>}/>
+                  </ListItem>
+                ))
+              : (query.data.wallet.transactions.length > 0
+                  ? query.data.wallet.transactions.map((tx: any) => (
+                      <Transaction key={tx.hash} transaction={tx}/>
+                    ))
+                  : <Typography variant="caption" sx={{ paddingLeft: 3 }}>No transactions yet</Typography>
+              )
             }
           </List>
         </Paper>
--- client/pages/receive.tsx
+++ client/pages/receive.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 import Layout from "../components/layout";
 import { gql, useQuery } from "@apollo/client";
-import { Box, Container, Grid, Paper, Typography, useMediaQuery } from "@mui/material";
+import { Box, Container, Grid, Paper, Typography, useMediaQuery, Skeleton } from "@mui/material";
 import QRCode from 'react-qr-code';
 
 const WalletQuery = gql`
@@ -18,25 +18,29 @@ const Receive: NextPage = () => {
   const qrFgColor = prefersDarkMode ? '#ffffff' : '#000000';
   const query = useQuery(WalletQuery);
 
+  const loading = query.loading || query.error || !query.data.wallet;
+
   let content;
 
-  if (query.loading) {
-    content = <p>Loading...</p>;
-  } else if (query.error) {
+  if (query.error) {
     content = <>
       <p>Something went wrong: {query.error.toString()}</p>
       <p>Got a 401 error? Please go to the settings tab to login.</p>
     </>;
-  } else if (!query.data.wallet) {
-    content = <p>You have no wallet. Please go to the settings page to make one.</p>;
   } else {
     content = <Container sx={{ marginY: 3 }}>
       <Paper variant="outlined" sx={{ textAlign: 'center', padding: 3 }}>
         <Box sx={{ margin: 3}}>
-          <QRCode value={`cashbite:${query.data.wallet.address}`} bgColor={qrBgColor} fgColor={qrFgColor} />
+          {loading
+            ? <Skeleton variant="rectangular" width={256} height={256} sx={{ marginX: 'auto' }} />
+            : <QRCode value={`cashbite:${query.data.wallet.address}`} bgColor={qrBgColor} fgColor={qrFgColor} />
+          }
         </Box>
         <Typography>Send coins to:</Typography>
-        <Typography sx={{ overflowX: 'auto' }}>{query.data.wallet.address}</Typography>
+        {loading
+          ? <Typography><Skeleton/></Typography>
+          : <Typography sx={{ overflowX: 'auto' }}>{query.data.wallet.address}</Typography>
+        }
       </Paper>
     </Container>;
   }
